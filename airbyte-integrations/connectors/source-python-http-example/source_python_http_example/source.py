feb04066c6401d1f3b27f7d4bf8c593c9c5dc8fb
Jagruti <tiwarijagruti1@gmail.com>
2022-12-04 11:37:49 +0530

    Completed until step 3. Stuck at step 4 of the documentation

---
+++ airbyte-integrations/connectors/source-python-http-example/source_python_http_example/source.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
+
+"""
+This file provides the necessary constructs to interpret a provided declarative YAML configuration file into
+source connector.
+
+WARNING: Do not modify this file.
+"""
+
+
+# Declarative Source
+class SourcePythonHttpExample(YamlDeclarativeSource):
+    def __init__(self):
+        super().__init__(**{"path_to_yaml": "python_http_example.yaml"})
+
+    def check_connection(self, logger, config) -> tuple[bool, any]:
+        accepted_currencies = {"USD", "JPY", "BGN", "CZK", "DKK"}  # assume these are the only allowed currencies
+        input_currency = config['base']
+        if input_currency not in accepted_currencies:
+            return False, f"Input currency {input_currency} is invalid. Please input one of the following currencies: {accepted_currencies}"
+        else:
+            return True, None
